a1b8e0cd8a83707e03e19d1eda10816ceea8a180
Singgih <gokonggokong71@gmail.com>
2021-04-23 18:48:14 +0700

    layout

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,32 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="flex">
+    <div
+      class="w-20 flex-none flex flex-col min-h-screen h-screen overflow-y-auto"
+    >
+      <div class="overflow-y-auto">
+        logos
+      </div>
+    </div>
+    <div class="flex flex-1 flex-col min-h-screen h-screen">
+      <div class="bg-red-500 flex">
+        <div class="bg-red-300 w-56 flex-none">Tailwind CSS</div>
+        <div class="">Rest of top bar</div>
+      </div>
+      <div class="flex-1 bg-blue-500 flex overflow-y-hidden">
+        <div class="bg-green-300 w-56 flex-none flex flex-col justify-between">
+          <div class="overflow-y-auto">left side</div>
+          <div class="bg-red-500">Bottom</div>
+        </div>
+        <div class="flex-1 flex justify-between">
+          <div class="bg-indigo-300 flex-1 flex flex-col justify-between">
+            <div class="overflow-y-auto">middle chat</div>
+            <div class="bg-gray-700">Chat input goes here</div>
+          </div>
+          <div class="bg-indigo-600 w-56 flex-none overflow-y-auto">
+            right side
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
-
-<script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
-export default {
-  name: "Home",
-  components: {
-    HelloWorld,
-  },
-};
-</script>
